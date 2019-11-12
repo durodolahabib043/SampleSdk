24793c229e3f11ae239d109e65708334e92358f2
habib <durodolahabib043@gmail.com>
2019-11-12 09:56:03 -0500

    first commit

---
+++ SampleSdk/NewSdA.swift
@@ -0,0 +1,30 @@
+//
+//  NewSdA.swift
+//  SampleSdk
+//
+//  Created by Habib Durodola on 2019-09-19.
+//  Copyright © 2019 Habib Durodola. All rights reserved.
+//
+
+import Foundation
+
+public class NewSdA {
+    public init() {}
+    let decodedURL = "olawepo://Great!%20It%20works!"
+    let decodedURLBBB = "olawepo://Great!%20It%20workadsasdsdsds!"
+   public func displayA(){
+        if let url = URL(string: decodedURL) {
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url)
+            }
+        }
+    }
+
+    public func displayB(){
+        if let url = URL(string: decodedURLBBB) {
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url)
+            }
+        }
+    }
+}
---
+++ SampleSdk/SampleSdk.h
@@ -0,0 +1,19 @@
+//
+//  SampleSdk.h
+//  SampleSdk
+//
+//  Created by Habib Durodola on 2019-09-19.
+//  Copyright © 2019 Habib Durodola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleSdk.
+FOUNDATION_EXPORT double SampleSdkVersionNumber;
+
+//! Project version string for SampleSdk.
+FOUNDATION_EXPORT const unsigned char SampleSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleSdk/PublicHeader.h>
+
+
